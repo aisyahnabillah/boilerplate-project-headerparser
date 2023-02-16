d97ed93116163035633da2c24bae32bb77d11da3
Aisyah Nabillah <aisyahnabillah97@gmail.com>
2023-02-16 15:02:28 +0700

    add endpoint for whoami route

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Set up a route to return IP address, language, and software information
+app.get('/api/whoami', function (req, res) {
+  // Get the IP address from the request object
+  var ip = req.ip;
+
+  // Get the preferred language from the Accept-Language header in the request
+  var language = req.headers['accept-language'];
+
+  // Get the user agent string from the User-Agent header in the request
+  var software = req.headers['user-agent'];
+
+  // Create an object to hold the response data
+  var responseData = { ipaddress: ip, language: language, software: software };
+
+  // Send the response as JSON
+  res.json(responseData);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
